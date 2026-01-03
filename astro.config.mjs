4d1fecdc3cd56553b06521ba7d3d59a830423447
TheRaulH <raulherreracruz18@gmail.com>
2026-01-03 09:44:13 -0400

    fix: add Netlify adapter configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   base: "/",
   outDir: "./dist",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
